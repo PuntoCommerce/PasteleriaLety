c88ce7d797f7c77adea985b92e76dde643c8ea49
Myrna90 <myrnamares90@gmail.com>
2022-07-08 15:35:15 -0500

    Se agrego a product tile ocultar y mostrar descripción

--- cartridges/app_custom_lety/cartridge/static/default/js/hiddenBtn.js
+++ cartridges/app_custom_lety/cartridge/static/default/js/hiddenBtn.js
@@ -1,5 +1,53 @@
+const showChar = 10;
+const ellipsestext = "...";
 
+window.onload = () => {
+  const descriptions = document.querySelectorAll(".truncate p");
+  descriptions.forEach((el) => {
+    let content = el.textContent;
+    if (content.length >= showChar) {
+      let formattedText = content.substr(0, showChar);
+      let originalText = content;
+      var newHtml =
+        '<div class="truncate-text">' +
+        formattedText +
+        '<span class="moreellipses">' +
+        ellipsestext +
+        '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text d-none">' +
+        originalText +
+        '<a href="" class="moreless less">Less</a></span></div>';
+      el.insertAdjacentHTML("afterend", newHtml);
+    }
+  });
+  setTimeout(() => {
+    const descritptionBtn = document.querySelectorAll(".moreless.more");
+    descritptionBtn.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        var descriptionFormattedText = el.parentNode.parentNode;
+        var descriptionOriginalText =
+          descriptionFormattedText.nextElementSibling;
+          console.log(descriptionFormattedText,descriptionOriginalText)
+        descriptionOriginalText.classList.remove("d-none");
+        descriptionFormattedText.classList.remove("d-block");
+        descriptionFormattedText.classList.add("d-none");
+      });
+    });
+  }, 500);
 
-function blockInput() {
-    document.getElementsByClassName(search-icon).style="block";
-}
+  setTimeout(() => {
+    const descritptionBtn = document.querySelectorAll(".moreless.less");
+    descritptionBtn.forEach((el) => {
+      el.addEventListener("click", (e) => {
+        e.preventDefault();
+        var descriptionFormattedText = el.parentNode;
+        var descriptionOriginalText = descriptionFormattedText.previousSibling;
+        descriptionFormattedText.classList.add("d-none");
+        descriptionOriginalText.classList.remove("d-none");
+        descriptionOriginalText.classList.add("d-block");
+        console.log(descriptionFormattedText,descriptionOriginalText)
+      });
+    });
+  }, 500);
+
+};
