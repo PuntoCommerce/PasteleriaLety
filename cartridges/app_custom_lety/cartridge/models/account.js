7a016bb520da500876f1df150ca6d8de078faf14
Myrna90 <myrnamares90@gmail.com>
2022-10-25 14:05:17 -0500

    Controller account repair

--- cartridges/app_custom_lety/cartridge/models/account.js
+++ cartridges/app_custom_lety/cartridge/models/account.js
@@ -2,7 +2,7 @@ const base = module.superModule;
 
 function account(currentCustomer, addressModel, orderModel) {
   base.call(this, currentCustomer, addressModel, orderModel);
-  if (currentCustomer.raw.profile.custom.letyPuntosCard) {
+  if (currentCustomer.raw.profile.custom.hasOwnProperty('letyPuntosCard')) {
     this.LetyCard = currentCustomer.raw.profile.custom.letyPuntosCard;
   } else {
     this.LetyCard = undefined;
