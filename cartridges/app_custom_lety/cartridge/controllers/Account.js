a7811613c8fdd07b2db81d84f823baa7ecf261be
DanielE2099 <daniel.estrada@puntocommerce.com>
2022-11-08 20:38:51 -0600

    UpdateChangesAccountAndClubLety

--- cartridges/app_custom_lety/cartridge/controllers/Account.js
+++ cartridges/app_custom_lety/cartridge/controllers/Account.js
@@ -9,6 +9,32 @@ const ApiServiceLety = require("*/cartridge/scripts/jobs/api");
 
 server.extend(module.superModule);
 
+server.append(
+  'Show',
+  function (req, res, next) {
+      const viewData = res.getViewData();
+      let Func_MovimientosMembresia = ApiServiceLety.ApiLety(
+        "Func_MovimientosMembresia",
+        { Empresa: 1, s_IdMembresia: req.querystring.letyCard }
+      );
+    
+      let bloquearBoton = true;
+      if(Array.isArray(Func_MovimientosMembresia)) {
+        bloquearBoton = true;
+        let JsonMovimientosMembresia = JsonMovimientosMembresia = JSON.parse(Func_MovimientosMembresia);
+        let ListaMovimientos =JsonMovimientosMembresia.Func_MovimientosMembresia;
+      } else {
+        bloquearBoton = false;
+        
+      }
+
+      res.render('account/accountDashboard', {
+        bloquearBoton: bloquearBoton
+      });
+      next();
+  }
+);
+
 server.get("Saldo", server.middleware.https, function (req, res, next) {
   let letyCard = req.querystring.letyCard;
   let Func_DatosMembresia = ApiServiceLety.ApiLety("Func_DatosMembresia", {
@@ -19,7 +45,7 @@ server.get("Saldo", server.middleware.https, function (req, res, next) {
   let FechaAlta = 0;
   let StatusMembresia = 0;
  
-  if(Func_DatosMembresia){
+  if(typeof(Func_DatosMembresia.d_SaldoMembresia)){
     let JsonDatosMembresia = JSON.parse(Func_DatosMembresia);
     SaldoMembresia =
     JsonDatosMembresia.Func_DatosMembresia[0]["d_SaldoMembresia"];
@@ -43,14 +69,18 @@ server.get("Movimientos", server.middleware.https, function (req, res, next) {
   var PageMgr = require("dw/experience/PageMgr");
   var pageMetaHelper = require("*/cartridge/scripts/helpers/pageMetaHelper");
   var accountHelpers = require("*/cartridge/scripts/account/accountHelpers");
+  let ListaMovimientos = 0;
 
-  let Func_MovimientosMembresia = ApiServiceLety.ApiLety(
+
+ /* let Func_MovimientosMembresia = ApiServiceLety.ApiLety(
     "Func_MovimientosMembresia",
     { Empresa: 1, s_IdMembresia: req.querystring.letyCard }
   );
-  let JsonMovimientosMembresia = JsonMovimientosMembresia = JSON.parse(Func_MovimientosMembresia);
-  let ListaMovimientos =JsonMovimientosMembresia.Func_MovimientosMembresia;
 
+    let JsonMovimientosMembresia = JSON.parse(Func_MovimientosMembresia);
+    ListaMovimientos =JsonMovimientosMembresia.Func_MovimientosMembresia;*/
+
+  //res.render("account/movesLetyClub", { ListaMovimientos: {} });
   res.render("account/movesLetyClub", { ListaMovimientos: ListaMovimientos });
   next();
 });
