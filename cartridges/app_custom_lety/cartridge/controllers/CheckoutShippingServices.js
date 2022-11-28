eff6e483c92499bbdd5e1fd9a680805dc5c44fe8
Alejandro Monge <alejandro@puntocommerce.com>
2022-11-28 15:23:59 -0600

    fix dates and time over current datetime

--- cartridges/app_custom_lety/cartridge/controllers/CheckoutShippingServices.js
+++ cartridges/app_custom_lety/cartridge/controllers/CheckoutShippingServices.js
@@ -17,6 +17,23 @@ server.append("SubmitShipping", (req, res, next) => {
 
   let viewData = res.getViewData();
 
+  const formFields = COHelpers.validateFields({
+    email: shipping.customPickUp.email,
+    date: shipping.datetime.date,
+    time: shipping.datetime.time,
+  });
+
+  if (Object.keys(formFields).length > 0) {
+    req.session.privacyCache.set(currentBasket.defaultShipment.UUID, "invalid");
+
+    res.json({
+      form: shipping,
+      fieldErrors: [formFields],
+      serverErrors: [],
+      error: true,
+    });
+  }
+
   Transaction.wrap(() => {
     let pickUpId = viewData.storeId;
     currentBasket.setCustomerEmail(shipping.customPickUp.email.value);
