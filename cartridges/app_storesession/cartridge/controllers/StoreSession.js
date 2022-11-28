ff167da929a915be6988229962ea3d6363b2bc0c
Alejandro Monge <alejandro@puntocommerce.com>
2022-11-28 12:54:05 -0600

    default selected store - Checkout

--- cartridges/app_storesession/cartridge/controllers/StoreSession.js
+++ cartridges/app_storesession/cartridge/controllers/StoreSession.js
@@ -8,44 +8,18 @@ const Session = require("dw/system/Session");
 const { closestStore } = require("~/cartridge/scripts/helpers/distance");
 
 server.get("Start", (req, res, next) => {
-  //let variable = req;
   const currentSite = Site.getCurrent();
   const apikey = currentSite.getCustomPreferenceValue("mapAPI");
-  const isSessionStoreMandatory = currentSite.getCustomPreferenceValue(
-    "isSessionStoreMandatory"
-  );
 
   const storeId = req.session.raw.privacy.storeId;
-  let storeName = undefined;
-  let storeAddress1 = undefined;
-  let storeAddress2 = undefined;
-  let storeAddress1f = undefined;
-  let storeAddress2f = undefined;
+  let store;
   if (storeId) {
-    const store = StoreMgr.getStore(storeId);
-    storeName = store.name;
-    if(store.address1 === null){
-        storeAddress1 = "No hay Dirección";
-    }else{
-      storeAddress1 = store.address1;
-    }
-    if(store.address2 === null){
-      storeAddress2 = "No hay Dirección";
-    }else{
-      storeAddress2 = store.address2;
-    }
-    let arr1 = storeAddress1.split(',');
-    let arr2 = storeAddress2.split(',');
-    storeAddress1f = arr1[0];
-    storeAddress2f = arr2[0];
+    store = StoreMgr.getStore(storeId);
   }
 
   res.render("storesession/session", {
-    // apikey: apikey,
-    isSessionStoreMandatory,
-    storeName: storeName,
-    storeAddress1: storeAddress1f,
-    storeAddress2: storeAddress2f
+    apikey: apikey,
+    store: store,
   });
   next();
 });
@@ -56,7 +30,6 @@ server.post("CleanStore", (req, res, next) => {
 });
 
 server.post("SetStore", (req, res, next) => {
-  
   // let CustomerMgr = require('dw/customer/CustomerMgr');
   // let AddressModel = require('*/cartridge/models/address');
   // let collections = require('*/cartridge/scripts/util/collections');
@@ -65,11 +38,9 @@ server.post("SetStore", (req, res, next) => {
   // // Lista de Direcciones
   // let rawAddressBook = customer.addressBook.getAddresses();
 
-
   // // Sesion en cache del usuario
   // let userSession = req.session.privacyCache.set("storeId", storeId);
 
-  
   const stores = SystemObjectMgr.querySystemObjects(
     "Store",
     "latitude != {0} AND longitude != {0}",
@@ -90,9 +61,6 @@ server.post("SetStore", (req, res, next) => {
   next();
 });
 
-
-
 module.exports = server.exports();
-
 
 //1 enpieza
