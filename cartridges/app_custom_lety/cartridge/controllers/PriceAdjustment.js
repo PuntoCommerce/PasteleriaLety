c9dfa88abf616a56c205df6c42da95195837051a
Criss117Devs <nevve48@gmail.com>
2022-11-30 13:58:49 -0600

    price adjustment ready, pending accessories

--- cartridges/app_custom_lety/cartridge/controllers/PriceAdjustment.js
+++ cartridges/app_custom_lety/cartridge/controllers/PriceAdjustment.js
@@ -1,82 +1,89 @@
 const server = require("server");
- 
- 
 var BasketMgr = require('dw/order/BasketMgr');
 var Transaction = require('dw/system/Transaction');
 var AmountDiscount = require('dw/campaign/AmountDiscount');
 var ProductLineItem = require('dw/order/ProductLineItem');
- 
-// const currentBasket = BasketMgr.getCurrentBasket();
- 
+
+const renderTemplateHelper = require("*/cartridge/scripts/renderTemplateHelper");
+var OrderModel = require('*/cartridge/models/order');
+var Locale = require("dw/util/Locale");
+
+var COHelpers = require("*/cartridge/scripts/checkout/checkoutHelpers");
 server.post("LetyPuntos", (req, res, next) => {
-  
-   const currentBasket = BasketMgr.getCurrentBasket();
- 
-   const {toAjustment, member} = req.form;
- 
-   let result;
-   let code;
-   let err;
-  
-   try {
- 
-       Transaction.wrap( function (params) {
-           currentBasket.createPriceAdjustment("promoprueba", AmountDiscount(Number(toAjustment)));
-       });
- 
-       code = 0;
-       err = "Sin error";
-      
-   } catch (error) {
-       err = error;
-       code = 1;
-       result = "";
-   }
- 
-   res.json({
-       body: toAjustment,
-       err,
-       code
-   })
- 
-   next();
+
+    const currentBasket = BasketMgr.getCurrentBasket();
+    const {
+        toAjustment,
+        member
+    } = req.form;
+    let result;
+    let code;
+    let err;
+    try {
+        Transaction.wrap(function (params) {
+            currentBasket.createPriceAdjustment("promoprueba", AmountDiscount(Number(toAjustment)));
+        });
+
+        COHelpers.recalculateBasket(currentBasket); // update
+        code = 0;
+        err = "Sin error";
+
+    } catch (error) {
+        err = error;
+        code = 1;
+        result = "";
+    }
+
+    var currentLocale = Locale.getLocale(req.locale.id);
+
+    var orderModel = new OrderModel(
+        currentBasket, {
+            usingMultiShipping: false,
+            shippable: true,
+            countryCode: currentLocale.country,
+            containerView: 'basket'
+        }
+    );
+    let renderedTotas = renderTemplateHelper.getRenderedHtml({
+            order: orderModel
+        },
+        "checkout/orderTotalSummary"
+    );
+
+    // dela forma viejita.
+    res.json({
+        body: toAjustment,
+        err,
+        code,
+        renderedTotas,
+    })
+    next();
 });
- 
 server.post("RemoveLetyPuntos", (req, res, next) => {
- 
-   /*
-       Si se intenta remover el ajuste de precio si haber agregado antes, lanzara una excepcion.
-       validar que solo aplique si existe.
-   */
- 
-   const currentBasket = BasketMgr.getCurrentBasket();
- 
-   let result;
-   let code;
-   let err;
- 
-   try {
- 
-       Transaction.wrap( function (params) {
-           result = currentBasket.removePriceAdjustment(currentBasket.priceAdjustments[0]);
-       });
- 
-       code = 0;
-       err = "Sin error";
- 
-   } catch (error) {
-       err = error;
-       code = 1;
-       result = "";
-   }
- 
-   res.json({
-       err,
-       code,
-       response: result
-     });
- 
-   next();
+    /*
+        Si se intenta remover el ajuste de precio si haber agregado antes, lanzara una excepcion.
+        validar que solo aplique si existe.
+    */
+    const currentBasket = BasketMgr.getCurrentBasket();
+    let result;
+    let code;
+    let err;
+    try {
+        Transaction.wrap(function (params) {
+            result = currentBasket.removePriceAdjustment(currentBasket.priceAdjustments[0]);
+        });
+        code = 0;
+        err = "Sin error";
+    } catch (error) {
+        err = error;
+        code = 1;
+        result = "";
+    }
+    res.json({
+        err,
+        code,
+        response: result
+    });
+    next();
 });
- 
 module.exports = server.exports();
