14de4f1a5ca0aab6a7143fa0e33446c0443f7713
Alejandro Monge <alejandro@puntocommerce.com>
2022-12-12 17:37:03 -0600

    init maps from server

---
+++ cartridges/app_custom_lety/cartridge/scripts/googleMaps/service.js
@@ -0,0 +1,90 @@
+const ServiceCredential = require("dw/svc/ServiceCredential");
+const LocalServiceRegistry = require("dw/svc/LocalServiceRegistry");
+const Resource = require("dw/web/Resource");
+const Site = require("dw/system/Site");
+const collections = require("*/cartridge/scripts/util/collections");
+
+/**
+ * Create URL for a call
+ * @param  {dw.svc.ServiceCredential} credential current service credential
+ * @param  {string} path REST action endpoint
+ * @returns {string} url for a call
+ */
+function getUrlPath(credential, path) {
+  var url = credential.URL;
+  if (!url.match(/.+\/$/)) {
+    url += "/";
+  }
+  url += path;
+  return url;
+}
+
+/** createRequest callback for a service
+ * @param  {dw.svc.Service} service service instance
+ * @param  {Object} data call data with path, method, body for a call or createToken in case of recursive call
+ * @returns {string} request body
+ */
+function createRequest(service, data) {
+  var credential = service.configuration.credential;
+  if (!(credential instanceof ServiceCredential)) {
+    var { msgf } = Resource;
+    throw new Error(
+      msgf("service.nocredentials", "pruebaerrors", null, serviceName)
+    );
+  }
+  var { path, method, params, token } = data;
+
+  let mapAPI = Site.getCurrent().getCustomPreferenceValue("mapAPI");
+
+  service.setURL(getUrlPath(credential, path));
+  service.addHeader("Content-Type", "application/json");
+  service.setRequestMethod("GET");
+  service.addParam("key", credential.password);
+  service.setAuthentication("NONE");
+
+  params.forEach(({ key, value }) => service.addParam(key, value));
+
+  return "";
+}
+
+module.exports = (function () {
+  var { msgf } = Resource;
+  var restService;
+  try {
+    restService = LocalServiceRegistry.createService("Google_Maps", {
+      createRequest: createRequest,
+      parseResponse: function (_, httpClient) {
+        return JSON.parse(httpClient.getText());
+      },
+      filterLogMessage: function (msg) {
+        return msg;
+      },
+      getRequestLogMessage: function (request) {
+        return request;
+      },
+      getResponseLogMessage: function (response) {
+        return response.text;
+      },
+    });
+  } catch (error) {
+    return error;
+    // throw new Error();
+  }
+
+  return {
+    call: function (data) {
+      var result;
+      try {
+        result = restService.setThrowOnError().call(data);
+      } catch (error) {
+        return { error: true, detail: error };
+        throw new Error();
+      }
+      if (result.isOk()) {
+        return restService.response;
+      } else {
+        return { error: true, detail: result };
+      }
+    },
+  };
+})();
