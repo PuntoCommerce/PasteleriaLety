c9ca8c778b09c3c3de28153c79f56c13e45c8992
Myrna90 <myrnamares90@gmail.com>
2022-12-26 15:55:08 -0600

    Change in text register

--- cartridges/app_storefront_base/cartridge/controllers/Account.js
+++ cartridges/app_storefront_base/cartridge/controllers/Account.js
@@ -276,7 +276,7 @@ server.post(
                             registrationForm.form.customer.email.valid = false;
                             registrationForm.form.customer.emailconfirm.valid = false;
                             registrationForm.form.customer.email.error =
-                                Resource.msg('error.message.username.invalid.regist', 'forms', null);
+                                Resource.msg('error.message.username.invalid', 'forms', null);
                         }
                     }
                 }
@@ -481,7 +481,7 @@ server.post(
                     if (!status.error) {
                         formInfo.profileForm.customer.email.valid = false;
                         formInfo.profileForm.customer.email.error =
-                            Resource.msg('error.message.username.invalid.regist', 'forms', null);
+                            Resource.msg('error.message.username.invalid', 'forms', null);
                     }
 
                     delete formInfo.profileForm;
