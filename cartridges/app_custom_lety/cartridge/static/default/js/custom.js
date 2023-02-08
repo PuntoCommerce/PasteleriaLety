0237922b57b7efd3aa9c59e1174491bb51610b5e
Myrna90 <myrnamares90@gmail.com>
2023-02-08 17:16:22 -0600

    Added google tag manager code and analytics

--- cartridges/app_custom_lety/cartridge/static/default/js/custom.js
+++ cartridges/app_custom_lety/cartridge/static/default/js/custom.js
@@ -293,6 +293,14 @@ if(viewPassword) {
   });
 }
 
+window.onload=function(){
+  var pos=window.name || 0;
+  window.scrollTo(0,pos);
+  }
+  window.onunload=function(){
+  window.name=self.pageYOffset || (document.documentElement.scrollTop+document.body.scrollTop);
+  }
+
 // Seleccionamos el botón con id 'btn-compartir' 
 //const shareButton = document.getElementById("btn-compartir");
  
