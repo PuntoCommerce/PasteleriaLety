fb1e2514c84c5204bb16b9271323d2e5cb3f27cc
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-16 08:23:09 -0600

    comment condition to add word No.

--- cartridges/app_custom_lety/cartridge/static/default/js/customAddress.js
+++ cartridges/app_custom_lety/cartridge/static/default/js/customAddress.js
@@ -16,34 +16,34 @@
     feedBack.innerHTML = "";
   };
 
-  inputAddressOne.addEventListener("change", (e) => {
-    clearError();
-    let address = e.target.value;
-    address = address.toLowerCase();
-    if (!address.includes(INT_STRING)) {
-      return setError("Debes incluir la palabra 'No.' para separar el numero.");
-    }
-    try {
-      let [street, interior] = address.split(INT_STRING);
-      if (!street) {
-        return setError("Debes agregar una calle antes de numero.");
-      }
-      if (!interior) {
-        return setError("Debes agregar un numero despues de 'No.'");
-      }
+  // inputAddressOne.addEventListener("change", (e) => {
+  //   clearError();
+  //   let address = e.target.value;
+  //   address = address.toLowerCase();
+  //   if (!address.includes(INT_STRING)) {
+  //     return setError("Debes incluir la palabra 'No.' para separar el numero.");
+  //   }
+  //   try {
+  //     let [street, interior] = address.split(INT_STRING);
+  //     if (!street) {
+  //       return setError("Debes agregar una calle antes de numero.");
+  //     }
+  //     if (!interior) {
+  //       return setError("Debes agregar un numero despues de 'No.'");
+  //     }
 
-      if (interior.includes(EXT_STRING)) {
-        let [intNumber, exterior] = interior.split(EXT_STRING);
-        if (!exterior) {
-          return setError("Debes agregar un numero despues de 'Ext.'");
-        }
-        if (!intNumber) {
-          return setError("Debes agregar una calle antes de numero.");
-        }
-      }
-      button.disabled = false;
-    } catch (error) {
-      return setError("Error en el formato de la direccion.");
-    }
-  });
+  //     if (interior.includes(EXT_STRING)) {
+  //       let [intNumber, exterior] = interior.split(EXT_STRING);
+  //       if (!exterior) {
+  //         return setError("Debes agregar un numero despues de 'Ext.'");
+  //       }
+  //       if (!intNumber) {
+  //         return setError("Debes agregar una calle antes de numero.");
+  //       }
+  //     }
+  //     button.disabled = false;
+  //   } catch (error) {
+  //     return setError("Error en el formato de la direccion.");
+  //   }
+  // });
 })();
