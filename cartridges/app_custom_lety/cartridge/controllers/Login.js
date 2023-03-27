9bede15bbddeb94ba110eaf6e8d4fb66b3d9317f
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-27 11:11:38 -0600

    create membership form and send the attributes in controller

---
+++ cartridges/app_custom_lety/cartridge/controllers/Login.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**
+ * @namespace Login
+ */
+
+var server = require('server');
+server.extend(module.superModule)
+
+var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+
+/**
+ * Login-Show : This endpoint is called to load the login page
+ * @name Base/Login-Show
+ * @function
+ * @memberof Login
+ * @param {middleware} - consentTracking.consent
+ * @param {middleware} - server.middleware.https
+ * @param {middleware} - csrfProtection.generateToken
+ * @param {querystringparameter} - rurl - Redirect URL
+ * @param {querystringparameter} - action - Action on submit of Login Form
+ * @param {category} - sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.replace(
+    'Show',
+    consentTracking.consent,
+    server.middleware.https,
+    csrfProtection.generateToken,
+    function (req, res, next) {
+        var URLUtils = require('dw/web/URLUtils');
+        var Resource = require('dw/web/Resource');
+
+        var target = req.querystring.rurl || 1;
+
+        var rememberMe = false;
+        var userName = '';
+        var actionUrl = URLUtils.url('Account-Login', 'rurl', target);
+        var createAccountUrl = URLUtils.url('Account-SubmitRegistration', 'rurl', target).relative().toString();
+        var navTabValue = req.querystring.action;
+        var getMembershipUrl = URLUtils.url('Account-GetDataFromEvo').relative().toString();
+
+        if (req.currentCustomer.credentials) {
+            rememberMe = true;
+            userName = req.currentCustomer.credentials.username;
+        }
+
+        var breadcrumbs = [
+            {
+                htmlValue: Resource.msg('global.home', 'common', null),
+                url: URLUtils.home().toString()
+            }
+        ];
+
+        var profileForm = server.forms.getForm('profile');
+        var getMembershipForm = server.forms.getForm('customerEvo');
+        profileForm.clear();
+
+        res.render('/account/login', {
+            navTabValue: navTabValue || 'login',
+            rememberMe: rememberMe,
+            userName: userName,
+            actionUrl: actionUrl,
+            profileForm: profileForm,
+            breadcrumbs: breadcrumbs,
+            oAuthReentryEndpoint: 1,
+            createAccountUrl: createAccountUrl,
+            membershipForm: getMembershipForm,
+            getMembershipUrl: getMembershipUrl
+        });
+
+        next();
+    }
+);
+
+
+module.exports = server.exports();
