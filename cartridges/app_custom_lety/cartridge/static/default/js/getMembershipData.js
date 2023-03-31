21a78e1fa785b2c207f8f824be191f94a45e3edb
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-03-31 10:22:55 -0600

    create function close modal and change message on submit form

--- cartridges/app_custom_lety/cartridge/static/default/js/getMembershipData.js
+++ cartridges/app_custom_lety/cartridge/static/default/js/getMembershipData.js
@@ -7,8 +7,12 @@
     const url = form[0].action
     const dataForm = new FormData()
     const data = Object.fromEntries(new FormData(e.target).entries());
-    dataForm.append('membership', data.dwfrm_customerEvo_membershipId)
+    const message = document.getElementById('form-membershipId-confirm-error')
+    const successMessage = document.getElementById('form-membershipId-confirm-success').getAttribute('data-message');
+    const alertMessage = document.getElementById('form-membershipId-confirm-alert').getAttribute('data-message');
+
     var results = {}
+    dataForm.append('membership', data.dwfrm_customerEvo_membershipId)
 
     await fetch(url, {
       method: 'POST',
@@ -30,9 +34,23 @@
         Object.keys(results).forEach(function (innerKey) {
           $('input[name="' + innerKey + '"]').val(results[innerKey]);
         });
-        $('#form-membershipId-confirm-error').append(successMessage);
-        $('#form-membershipId-confirm-error').removeClass('invalid-feedback');
+        message.innerHTML = successMessage
+
+        $('.membership-button').attr('disabled', 'true')
+        $('.closeModal').attr('disabled', 'true')
+
+        setTimeout(() => {
+          $('.getMembershipData').addClass('d-none')
+        }, 1500)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        message.innerHTML = alertMessage
+      })
   })
 })()
+
+function closeModalMembership() {
+  const modal = document.querySelector('.getMembershipData');
+  modal.classList.add('d-none')
+}
