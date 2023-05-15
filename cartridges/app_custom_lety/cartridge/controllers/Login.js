e81998b97dcc1c96268c5d07565331d9610690bf
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-05-15 10:48:21 -0600

    use the endpoint to crete new users with google and send the welcome email

--- cartridges/app_custom_lety/cartridge/controllers/Login.js
+++ cartridges/app_custom_lety/cartridge/controllers/Login.js
@@ -9,6 +9,7 @@ server.extend(module.superModule)
 
 var csrfProtection = require('*/cartridge/scripts/middleware/csrf');
 var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+var accountHelpers = require('*/cartridge/scripts/helpers/accountHelpers');
 
 /**
  * Login-Show : This endpoint is called to load the login page
@@ -75,5 +76,139 @@ server.replace(
     }
 );
 
+server.replace('OAuthReentry', server.middleware.https, consentTracking.consent, function (req, res, next) {
+    var URLUtils = require('dw/web/URLUtils');
+    var oauthLoginFlowMgr = require('dw/customer/oauth/OAuthLoginFlowMgr');
+    var CustomerMgr = require('dw/customer/CustomerMgr');
+    var Transaction = require('dw/system/Transaction');
+    var Resource = require('dw/web/Resource');
+
+    var destination = req.session.privacyCache.store.oauthLoginTargetEndPoint;
+
+    var finalizeOAuthLoginResult = oauthLoginFlowMgr.finalizeOAuthLogin();
+    if (!finalizeOAuthLoginResult) {
+        res.redirect(URLUtils.url('Login-Show'));
+        return next();
+    }
+
+    var response = finalizeOAuthLoginResult.userInfoResponse.userInfo;
+    var oauthProviderID = finalizeOAuthLoginResult.accessTokenResponse.oauthProviderId;
+
+    if (!oauthProviderID) {
+        res.render('/error', {
+            message: Resource.msg('error.oauth.login.failure', 'login', null)
+        });
+
+        return next();
+    }
+
+    if (!response) {
+        res.render('/error', {
+            message: Resource.msg('error.oauth.login.failure', 'login', null)
+        });
+
+        return next();
+    }
+
+    var externalProfile = JSON.parse(response);
+    if (!externalProfile) {
+        res.render('/error', {
+            message: Resource.msg('error.oauth.login.failure', 'login', null)
+        });
+
+        return next();
+    }
+
+    var userID = externalProfile.id || externalProfile.uid;
+    if (!userID) {
+        res.render('/error', {
+            message: Resource.msg('error.oauth.login.failure', 'login', null)
+        });
+
+        return next();
+    }
+
+    var authenticatedCustomerProfile = CustomerMgr.getExternallyAuthenticatedCustomerProfile(
+        oauthProviderID,
+        userID
+    );
+
+    if (!authenticatedCustomerProfile) {
+        // Create new profile
+        Transaction.wrap(function () {
+            var newCustomer = CustomerMgr.createExternallyAuthenticatedCustomer(
+                oauthProviderID,
+                userID
+            );
+
+            authenticatedCustomerProfile = newCustomer.getProfile();
+            var firstName;
+            var lastName;
+            var email;
+
+            // Google comes with a 'name' property that holds first and last name.
+            if (typeof externalProfile.name === 'object') {
+                firstName = externalProfile.name.givenName;
+                lastName = externalProfile.name.familyName;
+            } else {
+                // The other providers use one of these, GitHub has just a 'name'.
+                firstName = externalProfile['first-name']
+                    || externalProfile.first_name
+                    || externalProfile.name;
+
+                lastName = externalProfile['last-name']
+                    || externalProfile.last_name
+                    || externalProfile.name;
+            }
+
+            email = externalProfile['email-address'] || externalProfile.email;
+
+            if (!email) {
+                var emails = externalProfile.emails;
+
+                if (emails && emails.length) {
+                    email = externalProfile.emails[0].value;
+                }
+            }
+
+            authenticatedCustomerProfile.setFirstName(firstName);
+            authenticatedCustomerProfile.setLastName(lastName);
+            authenticatedCustomerProfile.setEmail(email);
+        });
+
+        const birthDay = new Date().toLocaleDateString("en-US");
+        const lastName = externalProfile.family_name.split(' ', 2)
+        const secondLastName = lastName[1] ? lastName[1] :  ''
+
+        var resultForm = {
+            firstName: externalProfile.given_name,
+            lastName: lastName[0],
+            sLastName: secondLastName,
+            email: externalProfile.email,
+            birthDay: birthDay,
+            phone: '8111111111'
+        }
+
+        accountHelpers.sendCreateAccountEmail(resultForm);
+        accountHelpers.addUpdateExternalAccount(resultForm);
+    }
+
+    var credentials = authenticatedCustomerProfile.getCredentials();
+    if (credentials.isEnabled()) {
+        Transaction.wrap(function () {
+            CustomerMgr.loginExternallyAuthenticatedCustomer(oauthProviderID, userID, false);
+        });
+    } else {
+        res.render('/error', {
+            message: Resource.msg('error.oauth.login.failure', 'login', null)
+        });
+
+        return next();
+    }
+
+    res.redirect(URLUtils.url(destination));
+
+    return next();
+});
 
 module.exports = server.exports();
