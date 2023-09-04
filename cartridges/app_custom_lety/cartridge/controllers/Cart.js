c5759658976424f3ae7e21ec5abd8ef3f58ac1d4
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-09-04 17:28:45 -0600

    feature/product quantity validation in special products

--- cartridges/app_custom_lety/cartridge/controllers/Cart.js
+++ cartridges/app_custom_lety/cartridge/controllers/Cart.js
@@ -4,150 +4,170 @@ server.extend(module.superModule);
 const inventory = require("*/cartridge/scripts/middlewares/inventory");
 
 server.append("Show", (req, res, next) => {
-  if(req.querystring.error) {
-    let viewData = res.getViewData();
-    viewData.valid.error = true;
-    viewData.valid.message = req.querystring.error;
-    res.setViewData(viewData);
-  }
-  next();
+    if (req.querystring.error) {
+        let viewData = res.getViewData();
+        viewData.valid.error = true;
+        viewData.valid.message = req.querystring.error;
+        res.setViewData(viewData);
+    }
+    next();
 })
 
 server.replace('AddProduct', inventory.checkOnlineInventory, function (req, res, next) {
-  var BasketMgr = require('dw/order/BasketMgr');
-  var Resource = require('dw/web/Resource');
-  var URLUtils = require('dw/web/URLUtils');
-  var Transaction = require('dw/system/Transaction');
-  var ProductMgr = require("dw/catalog/ProductMgr");
-  var CartModel = require('*/cartridge/models/cart');
-  var ProductLineItemsModel = require('*/cartridge/models/productLineItems');
-  var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
-  var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+    var BasketMgr = require('dw/order/BasketMgr');
+    var Resource = require('dw/web/Resource');
+    var URLUtils = require('dw/web/URLUtils');
+    var Transaction = require('dw/system/Transaction');
+    var ProductMgr = require("dw/catalog/ProductMgr");
+    var CartModel = require('*/cartridge/models/cart');
+    var ProductLineItemsModel = require('*/cartridge/models/productLineItems');
+    var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
+    var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
 
-  let viewData = res.getViewData();
-  if(viewData.error) {
-    res.json({
-      error: viewData.error,
-      message: viewData.message,
-    })
-    return next()
-  }
+    let viewData = res.getViewData();
+    if (viewData.error) {
+        res.json({
+            error: viewData.error,
+            message: viewData.message,
+        })
+        return next()
+    }
 
-  var currentBasket = BasketMgr.getCurrentOrNewBasket();
-  var previousBonusDiscountLineItems = currentBasket.getBonusDiscountLineItems();
-  var productId = req.form.pid;
-  var childProducts = Object.hasOwnProperty.call(req.form, 'childProducts')
-      ? JSON.parse(req.form.childProducts)
-      : [];
-  var options = req.form.options ? JSON.parse(req.form.options) : [];
-  var quantity;
-  var result;
-  var pidsObj;
-	
-  let productType = ProductMgr.getProduct(productId).custom.tipoproducto;
+    var currentBasket = BasketMgr.getCurrentOrNewBasket();
+    var previousBonusDiscountLineItems = currentBasket.getBonusDiscountLineItems();
+    var productId = req.form.pid;
+    var childProducts = Object.hasOwnProperty.call(req.form, 'childProducts')
+        ? JSON.parse(req.form.childProducts)
+        : [];
+    var options = req.form.options ? JSON.parse(req.form.options) : [];
+    var quantity;
+    var result;
+    var pidsObj;
 
-  if (currentBasket.allProductLineItems.length > 0) {
-      var productIds = [];
-      var allLineItems = currentBasket.allProductLineItems;
-      var collections = require("*/cartridge/scripts/util/collections");
-      collections.forEach(allLineItems, function (pli) {
-          productIds.push(pli.productID);
-      });
-      var CartProductType = ProductMgr.getProduct(productIds[0]).custom.tipoproducto;
-      if (productType !== CartProductType) {
-          res.json({
+    var productType = ProductMgr.getProduct(productId).custom.tipoproducto;
+    var itemsQ = Number(req.form.quantity);
+
+    if (productType === 'pedido especial' && itemsQ > 1) {
+        res.json({
             error: true,
-            message: Resource.msg("text.alert.split.order", "product", null)
-          });
-          return next();
-      }
-  }
+            message: Resource.msg("error.alert.one.item", "product", null)
+        });
 
-  if (currentBasket) {
-      Transaction.wrap(function () {
-          if (!req.form.pidsObj) {
-              quantity = parseInt(req.form.quantity, 10);
-              result = cartHelper.addProductToCart(
-                  currentBasket,
-                  productId,
-                  quantity,
-                  childProducts,
-                  options
-              );
-          } else {
-              // product set
-              pidsObj = JSON.parse(req.form.pidsObj);
-              result = {
-                  error: false,
-                  message: Resource.msg('text.alert.addedtobasket', 'product', null)
-              };
+        return next();
+    }
 
-              pidsObj.forEach(function (PIDObj) {
-                  quantity = parseInt(PIDObj.qty, 10);
-                  var pidOptions = PIDObj.options ? JSON.parse(PIDObj.options) : {};
-                  var PIDObjResult = cartHelper.addProductToCart(
-                      currentBasket,
-                      PIDObj.pid,
-                      quantity,
-                      childProducts,
-                      pidOptions
-                  );
-                  if (PIDObjResult.error) {
-                      result.error = PIDObjResult.error;
-                      result.message = PIDObjResult.message;
-                  }
-              });
-          }
-          if (!result.error) {
-              cartHelper.ensureAllShipmentsHaveMethods(currentBasket);
-              basketCalculationHelpers.calculateTotals(currentBasket);
-          }
-      });
-  }
+    if (currentBasket.allProductLineItems.length > 0) {
+        var productIds = [];
+        var allLineItems = currentBasket.allProductLineItems;
+        var collections = require("*/cartridge/scripts/util/collections");
+        collections.forEach(allLineItems, function (pli) {
+            productIds.push(pli);
+        });
+        var CartProductType = ProductMgr.getProduct(productIds[0].productID).custom.tipoproducto;
 
-  var quantityTotal = ProductLineItemsModel.getTotalQuantity(currentBasket.productLineItems);
-  var cartModel = new CartModel(currentBasket);
+        if (productType !== CartProductType) {
+            res.json({
+                error: true,
+                message: Resource.msg("text.alert.split.order", "product", null)
+            });
+            return next();
+        }
 
-  var urlObject = {
-      url: URLUtils.url('Cart-ChooseBonusProducts').toString(),
-      configureProductstUrl: URLUtils.url('Product-ShowBonusProducts').toString(),
-      addToCartUrl: URLUtils.url('Cart-AddBonusProducts').toString()
-  };
+        if (CartProductType === 'pedido especial' && productIds[0].quantity.value === 1) {
+            res.json({
+                error: true,
+                message: Resource.msg("error.alert.one.item", "product", null)
+            });
 
-  var newBonusDiscountLineItem =
-      cartHelper.getNewBonusDiscountLineItem(
-          currentBasket,
-          previousBonusDiscountLineItems,
-          urlObject,
-          result.uuid
-      );
-  if (newBonusDiscountLineItem) {
-      var allLineItems = currentBasket.allProductLineItems;
-      var collections = require('*/cartridge/scripts/util/collections');
-      collections.forEach(allLineItems, function (pli) {
-          if (pli.UUID === result.uuid) {
-              Transaction.wrap(function () {
-                  pli.custom.bonusProductLineItemUUID = 'bonus'; // eslint-disable-line no-param-reassign
-                  pli.custom.preOrderUUID = pli.UUID; // eslint-disable-line no-param-reassign
-              });
-          }
-      });
-  }
+            return next();
+        }
+    }
 
-  var reportingURL = cartHelper.getReportingUrlAddToCart(currentBasket, result.error);
+    if (currentBasket) {
+        Transaction.wrap(function () {
+            if (!req.form.pidsObj) {
+                quantity = parseInt(req.form.quantity, 10);
+                result = cartHelper.addProductToCart(
+                    currentBasket,
+                    productId,
+                    quantity,
+                    childProducts,
+                    options
+                );
+            } else {
+                // product set
+                pidsObj = JSON.parse(req.form.pidsObj);
+                result = {
+                    error: false,
+                    message: Resource.msg('text.alert.addedtobasket', 'product', null)
+                };
 
-  res.json({
-      reportingURL: reportingURL,
-      quantityTotal: quantityTotal,
-      message: result.message,
-      cart: cartModel,
-      newBonusDiscountLineItem: newBonusDiscountLineItem || {},
-      error: result.error,
-      pliUUID: result.uuid,
-      minicartCountOfItems: Resource.msgf('minicart.count', 'common', null, quantityTotal)
-  });
+                pidsObj.forEach(function (PIDObj) {
+                    quantity = parseInt(PIDObj.qty, 10);
+                    var pidOptions = PIDObj.options ? JSON.parse(PIDObj.options) : {};
+                    var PIDObjResult = cartHelper.addProductToCart(
+                        currentBasket,
+                        PIDObj.pid,
+                        quantity,
+                        childProducts,
+                        pidOptions
+                    );
+                    if (PIDObjResult.error) {
+                        result.error = PIDObjResult.error;
+                        result.message = PIDObjResult.message;
+                    }
+                });
+            }
+            if (!result.error) {
+                cartHelper.ensureAllShipmentsHaveMethods(currentBasket);
+                basketCalculationHelpers.calculateTotals(currentBasket);
+            }
+        });
+    }
 
-  next();
+    var quantityTotal = ProductLineItemsModel.getTotalQuantity(currentBasket.productLineItems);
+    var cartModel = new CartModel(currentBasket);
+
+    var urlObject = {
+        url: URLUtils.url('Cart-ChooseBonusProducts').toString(),
+        configureProductstUrl: URLUtils.url('Product-ShowBonusProducts').toString(),
+        addToCartUrl: URLUtils.url('Cart-AddBonusProducts').toString()
+    };
+
+    var newBonusDiscountLineItem =
+        cartHelper.getNewBonusDiscountLineItem(
+            currentBasket,
+            previousBonusDiscountLineItems,
+            urlObject,
+            result.uuid
+        );
+    if (newBonusDiscountLineItem) {
+        var allLineItems = currentBasket.allProductLineItems;
+        var collections = require('*/cartridge/scripts/util/collections');
+        collections.forEach(allLineItems, function (pli) {
+            if (pli.UUID === result.uuid) {
+                Transaction.wrap(function () {
+                    pli.custom.bonusProductLineItemUUID = 'bonus'; // eslint-disable-line no-param-reassign
+                    pli.custom.preOrderUUID = pli.UUID; // eslint-disable-line no-param-reassign
+                });
+            }
+        });
+    }
+
+    var reportingURL = cartHelper.getReportingUrlAddToCart(currentBasket, result.error);
+
+    res.json({
+        reportingURL: reportingURL,
+        quantityTotal: quantityTotal,
+        message: result.message,
+        cart: cartModel,
+        newBonusDiscountLineItem: newBonusDiscountLineItem || {},
+        error: result.error,
+        pliUUID: result.uuid,
+        minicartCountOfItems: Resource.msgf('minicart.count', 'common', null, quantityTotal)
+    });
+
+    next();
 });
 
 server.replace('UpdateQuantity', inventory.checkOnlineInventory, function (req, res, next) {
@@ -159,9 +179,13 @@ server.replace('UpdateQuantity', inventory.checkOnlineInventory, function (req, 
     var collections = require('*/cartridge/scripts/util/collections');
     var cartHelper = require('*/cartridge/scripts/cart/cartHelpers');
     var basketCalculationHelpers = require('*/cartridge/scripts/helpers/basketCalculationHelpers');
+    var collections = require("*/cartridge/scripts/util/collections");
+    var ProductMgr = require('dw/catalog/ProductMgr');
+
+    var productIds = [];
 
     let viewData = res.getViewData();
-    if(viewData.error) {
+    if (viewData.error) {
         res.setStatusCode(500);
         res.json({
             error: viewData.error,
@@ -171,6 +195,24 @@ server.replace('UpdateQuantity', inventory.checkOnlineInventory, function (req, 
     }
 
     var currentBasket = BasketMgr.getCurrentBasket();
+    var allLineItems = currentBasket.allProductLineItems;
+
+    collections.forEach(allLineItems, function (pli) {
+        productIds.push(pli);
+    });
+
+    var CartProductType = ProductMgr.getProduct(productIds[0].productID).custom.tipoproducto;
+
+    if (CartProductType === 'pedido especial' && productIds[0].quantity.value === 1) {
+        res.setStatusCode(500);
+        res.json({
+            error: true,
+            errorMessage: Resource.msg("error.alert.one.item", "product", null)
+        });
+
+        return next();
+    }
+
 
     if (!currentBasket) {
         res.setStatusCode(500);
@@ -275,7 +317,7 @@ server.get("OnlineInventory", (req, res, next) => {
     let pid = req.querystring.pid;
     let storeId = req.session.raw.privacy.storeId;
 
-    if(req.querystring.storeId) {
+    if (req.querystring.storeId) {
         storeId = req.querystring.storeId;
     }
 
