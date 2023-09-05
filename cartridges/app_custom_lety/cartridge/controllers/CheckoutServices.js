37204cd2139a22bb9ce8bf1782fa1a1bfd0dbf47
Jeancarlosayala28 <carlos.ayala@puntocommerce.com>
2023-09-04 23:31:38 -0600

    fix/validation on create order

--- cartridges/app_custom_lety/cartridge/controllers/CheckoutServices.js
+++ cartridges/app_custom_lety/cartridge/controllers/CheckoutServices.js
@@ -172,6 +172,58 @@ server.replace(
       return next();
     }
 
+    // Insert Information on evolution
+
+    var status = {};
+    try {
+      if (order.defaultShipment.shippingMethodID == "pickup") {
+        status = HO.sendPickupOrderToERP(order.orderNo);
+      } else {
+        const userExist = req.session.privacyCache.get("userExist");
+        status = HO.sendShippingOrderToERP(order.orderNo, req, userExist);
+      }
+
+    } catch (error) {
+
+      status.error = true;
+      status.message = JSON.stringify(error);
+
+      Transaction.wrap(function () {
+        order.custom.isError = true;
+        OrderMgr.failOrder(order, true);
+      });
+
+      res.json({
+        error: true,
+        errorMessage: Resource.msg("error.order.try.again", "checkout", null),
+      });
+      return next();
+    }
+
+    if (status.error) {
+      Transaction.wrap(() => {
+        order.custom.isError = true;
+        order.custom.errorDetail = status.message;
+      })
+    } else {
+      Transaction.wrap(() => {
+        order.custom.orderDetailJson = JSON.stringify(status.payload)
+      })
+    }
+
+    if(!order.custom.orderDetailJson){
+      Transaction.wrap(() => {
+        order.custom.isError = true;
+        OrderMgr.failOrder(order, true);
+      })
+
+      res.json({
+        error: true,
+        errorMessage: Resource.msg("error.order.try.again", "checkout", null),
+      });
+      return next();
+    }
+
     // Handles payment authorization
     var handlePaymentResult = COHelpers.handlePayments(order, order.orderNo);
 
@@ -268,31 +320,6 @@ server.replace(
         COHelpers.sendConfirmationEmailClient(order, req.locale.id, storeId),
         COHelpers.sendConfirmationEmailClientSecund(order, req.locale.id, storeId)
       ) : COHelpers.sendConfirmationEmail(order, req.locale.id, storeId)
-    }
-
-    let status = {};
-    try {
-      if (order.defaultShipment.shippingMethodID == "pickup") {
-        status = HO.sendPickupOrderToERP(order.orderNo);
-      } else {
-        const userExist = req.session.privacyCache.get("userExist");
-        status = HO.sendShippingOrderToERP(order.orderNo, req, userExist);
-      }
-    } catch (error) {
-      status.error = true;
-      status.message = JSON.stringify(error);
-    }
-
-    if (status.error) {
-      Transaction.wrap(() => {
-        order.custom.isError = true;
-        order.custom.errorDetail = status.message;
-        order.custom.orderDetailJson = JSON.stringify(status.payload)
-      })
-    }else{
-      Transaction.wrap(() => {
-        order.custom.orderDetailJson = JSON.stringify(status.payload)
-      })
     }
 
     // Reset usingMultiShip after successful Order placement
